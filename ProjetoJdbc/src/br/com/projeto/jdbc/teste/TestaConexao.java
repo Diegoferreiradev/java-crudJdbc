e6a15566a22e81aa907d538aae367376bccb24f2
Diegoferreiradev <diegoferreiradev@gmail.com>
2018-07-26 15:35:34 -0300

    Desenvolvimento da classe de Conexão

--- ProjetoJdbc/src/br/com/projeto/jdbc/teste/TestaConexao.java
+++ ProjetoJdbc/src/br/com/projeto/jdbc/teste/TestaConexao.java
@@ -10,7 +10,7 @@ public class TestaConexao {
 	public static void main(String[] args) throws SQLException {
 		
 		Connection connection = new ConnectionFactory().getConnection();
-		System.out.println("Conexão Aberta");
+		System.out.println("Conexão Aberta!");
 		
 		connection.close();
 	}
